ce41534d4c680af17c8438a12e10c1f6d6a02e39
styazhkoV <alexsandr.styazhkov@gmail.com>
2023-10-02 22:49:31 +0600

    1

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -12,46 +12,39 @@ public class PizzaController : ControllerBase
     public PizzaController()
     {  
     }
-[HttpGet]
-public ActionResult<List<Pizza>> GetAll() =>
-PizzaService.GetAll();
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() =>
+    PizzaService.GetAll();
 
-[HttpGet("id")]
-public ActionResult<Pizza> Get(int id)
-{
-    var pizza = PizzaService.Get(id);
-    if(pizza == null)
-    return NotFound();
-    return pizza;
-}
-[HttpPost]
-public IActionResult Create(Pizza pizza)
-{
-    PizzaService.Add(pizza);
-    return CreatedAtAction(nameof(Get), new {id = pizza.Id}, pizza);
-}
-[HttpPut("{id}")]
-public IActionResult Update(int id, Pizza pizza)
-{
-    if (id != pizza.Id)
-    return BadRequest();
-    var existingPizza = PizzaService.Get(id);
-    if(existingPizza is null)
-    return NotFound();
-    PizzaService.Update(pizza);
-    return NoContent();
-}
+    [HttpGet("id")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+        if(pizza == null)
+        return NotFound();
+        return pizza;
+    }
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new {id = pizza.Id}, pizza);
+    }
+    [HttpPut("{id}")]   
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        if (id != pizza.Id)
+        return BadRequest();
+        var existingPizza = PizzaService.Get(id);
+        if(existingPizza is null)
+        return NotFound();
+        PizzaService.Update(pizza);
+        return NoContent();
+    }
 
 [HttpDelete("{id}")]
 public IActionResult Delete (int id)
 {
-    var pizza = PizzaService.Get (id);
-    if (pizza is null)
-    return NotFound();
-    PizzaService.Delete();
-    return NoContent();
-}
-
-
-    // DELETE action https://customer2dev.kazincombank.kz/api/demandType?id=98254235&applicationId=98254235
+        var pizza
+    }
 }
--- Services/PizzaService.cs
+++ Services/PizzaService.cs
@@ -18,7 +18,14 @@ public static class PizzaService
     {
         pizza.Id = nextId++;
         Pizzas.Add(pizza);
-    } 
+    }
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+        if(pizza is null)
+        return;
+        Pizzas.Remove(pizza);
+    }
     public static void Update(Pizza pizza)
     {
         var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
